80d61a4c9750f03b515157bfacc9601632d487d6
ashfaq_founder <ashfaqofficial.ae@gmail.com>
2026-06-23 13:20:27 +0400

    Add env config for deployment

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://biqnznwwsslzaalsiuei.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpcW56bnd3c3NsemFhbHNpdWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxOTQzMzEsImV4cCI6MjA5Nzc3MDMzMX0.zrHySjoX8vH8f3bS05lMzIJeolTnx7MI2v1k7MShAe0'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
